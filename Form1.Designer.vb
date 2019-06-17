364e8409f40800c3f490d4a70678ebccbd8957cc
Dhaval Tejlavwala <dhaval.tejlavwala@cpal.com.au>
2019-06-17 17:10:59 +1000

    Initial Commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.NameCB = New System.Windows.Forms.ComboBox()
+        Me.AmountTB = New System.Windows.Forms.TextBox()
+        Me.AddBtn = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.ResetBtn = New System.Windows.Forms.Button()
+        Me.AnswerRTB = New System.Windows.Forms.RichTextBox()
+        Me.HisabBtn = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.AddPersonBtn = New System.Windows.Forms.Button()
+        Me.AddPersonTB = New System.Windows.Forms.TextBox()
+        Me.BackgroundWorker = New System.ComponentModel.BackgroundWorker()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'NameCB
+        '
+        Me.NameCB.FormattingEnabled = True
+        resources.ApplyResources(Me.NameCB, "NameCB")
+        Me.NameCB.Name = "NameCB"
+        '
+        'AmountTB
+        '
+        resources.ApplyResources(Me.AmountTB, "AmountTB")
+        Me.AmountTB.Name = "AmountTB"
+        '
+        'AddBtn
+        '
+        resources.ApplyResources(Me.AddBtn, "AddBtn")
+        Me.AddBtn.Name = "AddBtn"
+        Me.AddBtn.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.ResetBtn)
+        Me.Panel1.Controls.Add(Me.AnswerRTB)
+        Me.Panel1.Controls.Add(Me.HisabBtn)
+        Me.Panel1.Controls.Add(Me.AddBtn)
+        resources.ApplyResources(Me.Panel1, "Panel1")
+        Me.Panel1.Name = "Panel1"
+        '
+        'ResetBtn
+        '
+        resources.ApplyResources(Me.ResetBtn, "ResetBtn")
+        Me.ResetBtn.Name = "ResetBtn"
+        Me.ResetBtn.UseVisualStyleBackColor = True
+        '
+        'AnswerRTB
+        '
+        resources.ApplyResources(Me.AnswerRTB, "AnswerRTB")
+        Me.AnswerRTB.Name = "AnswerRTB"
+        '
+        'HisabBtn
+        '
+        resources.ApplyResources(Me.HisabBtn, "HisabBtn")
+        Me.HisabBtn.Name = "HisabBtn"
+        Me.HisabBtn.UseVisualStyleBackColor = True
+        '
+        'Panel2
+        '
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Controls.Add(Me.AddPersonBtn)
+        Me.Panel2.Controls.Add(Me.AddPersonTB)
+        resources.ApplyResources(Me.Panel2, "Panel2")
+        Me.Panel2.Name = "Panel2"
+        '
+        'AddPersonBtn
+        '
+        resources.ApplyResources(Me.AddPersonBtn, "AddPersonBtn")
+        Me.AddPersonBtn.Name = "AddPersonBtn"
+        Me.AddPersonBtn.UseVisualStyleBackColor = True
+        '
+        'AddPersonTB
+        '
+        resources.ApplyResources(Me.AddPersonTB, "AddPersonTB")
+        Me.AddPersonTB.Name = "AddPersonTB"
+        '
+        'BackgroundWorker
+        '
+        '
+        'Form1
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.AmountTB)
+        Me.Controls.Add(Me.NameCB)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.IsMdiContainer = True
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents NameCB As System.Windows.Forms.ComboBox
+    Friend WithEvents AmountTB As System.Windows.Forms.TextBox
+    Friend WithEvents AddBtn As System.Windows.Forms.Button
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents AddPersonBtn As System.Windows.Forms.Button
+    Friend WithEvents AddPersonTB As System.Windows.Forms.TextBox
+    Friend WithEvents BackgroundWorker As System.ComponentModel.BackgroundWorker
+    Friend WithEvents HisabBtn As System.Windows.Forms.Button
+    Friend WithEvents AnswerRTB As System.Windows.Forms.RichTextBox
+    Friend WithEvents ResetBtn As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,160 @@
+﻿Imports System.ComponentModel
+
+Public Class Form1
+
+    Private toolTip As New ToolTip()
+    Private PersonCounter As Integer = 0
+    Private AmountCounter As Integer = 0
+    Private TotalAmount As Decimal = 0.0
+    Private PersonAmountArray(100, 100) As Integer
+    Private Status As String = String.Empty
+    Private bw As BackgroundWorker = New BackgroundWorker
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        AddPersonTB.Select()
+    End Sub
+
+    Private Sub AddPersonBtn_Click(sender As Object, e As EventArgs) Handles AddPersonBtn.Click
+        If AddPersonTB.Text = "" Then
+            Exit Sub
+        End If
+        NameCB.Items.Insert(PersonCounter, AddPersonTB.Text)
+        Status &= AddPersonTB.Text & " Added" & Environment.NewLine
+        AddPersonTB.Text = ""
+        'ReDim Preserve PersonAmountArray(PersonCounter, AmountCounter)
+        PersonAmountArray(PersonCounter, 0) = 0
+        PersonCounter = PersonCounter + 1
+        AddPersonTB.Select()
+        AnswerRTB.Text = Status
+    End Sub
+
+    Private Sub AddBtn_Click(sender As Object, e As EventArgs) Handles AddBtn.Click
+        If Not IsNumeric(AmountTB.Text) Then
+            Status &= "Invalid Amount"
+            AnswerRTB.Text = Status
+            Exit Sub
+        End If
+        TotalAmount = TotalAmount + AmountTB.Text
+        Dim personId As Integer
+        Try
+            personId = NameCB.SelectedIndex
+        Catch ex As Exception
+            Exit Sub
+        End Try
+        PersonAmountArray(personId, AmountCounter) = AmountTB.Text
+        AmountCounter = AmountCounter + 1
+        NameCB.Select()
+        Status &= NameCB.SelectedItem.ToString & " Has Paid Rs." & AmountTB.Text & Environment.NewLine
+        AnswerRTB.Text = Status
+    End Sub
+
+    Private Sub HisabBtn_Click(sender As Object, e As EventArgs) Handles HisabBtn.Click
+        'If Not bw.IsBusy = True Then
+        '    bw.RunWorkerAsync()
+        'End If
+        Dim PerHeadAmount, IndividualAmount, ExtraDueAmount, SettlementAmount As Decimal
+        Dim ExtraDueArray(PersonCounter - 1) As Integer
+        Dim Hisab As String = "Total Amount is Rs." & TotalAmount
+        PerHeadAmount = Math.Round(TotalAmount / PersonCounter, 0)
+        Hisab &= Environment.NewLine
+        Hisab &= Environment.NewLine
+        Hisab &= "Each one has to pay Rs." & PerHeadAmount
+        Hisab &= Environment.NewLine
+        Hisab &= Environment.NewLine
+        For pCount = 0 To PersonCounter - 1
+            IndividualAmount = 0
+            For aCount = 0 To AmountCounter - 1
+                IndividualAmount = IndividualAmount + PersonAmountArray(pCount, aCount)
+            Next
+            ExtraDueAmount = IndividualAmount - PerHeadAmount
+            ExtraDueArray(pCount) = ExtraDueAmount
+            Hisab &= NameCB.Items(pCount).ToString() & " paid Rs." & IndividualAmount & "  =>  " & ExtraDueAmount
+            Hisab &= Environment.NewLine
+        Next
+        Hisab &= Environment.NewLine
+        For hCount = 0 To ExtraDueArray.Count - 1
+            SettlementAmount = 0
+            If ExtraDueArray(hCount) < 0 Then
+                For cCount = 0 To ExtraDueArray.Count - 1
+                    If ExtraDueArray(cCount) > 0 Then
+                        SettlementAmount = ExtraDueArray(cCount) + ExtraDueArray(hCount)
+                        If SettlementAmount = 0 Then
+                            Hisab &= NameCB.Items(hCount).ToString() & " has to pay Rs." & ExtraDueArray(hCount) * (-1) & " to " & NameCB.Items(cCount).ToString()
+                            Hisab &= Environment.NewLine
+                            ExtraDueArray(hCount) = SettlementAmount
+                            ExtraDueArray(cCount) = SettlementAmount
+                            Exit For
+                        ElseIf SettlementAmount < 0 Then
+                            Hisab &= NameCB.Items(hCount).ToString() & " has to pay Rs." & ExtraDueArray(cCount) & " to " & NameCB.Items(cCount).ToString()
+                            Hisab &= Environment.NewLine
+                            ExtraDueArray(hCount) = SettlementAmount
+                            ExtraDueArray(cCount) = SettlementAmount
+                            Exit For
+                        ElseIf SettlementAmount > 0 Then
+                            Hisab &= NameCB.Items(hCount).ToString() & " has to pay Rs." & ExtraDueArray(hCount) * (-1) & " to " & NameCB.Items(cCount).ToString()
+                            Hisab &= Environment.NewLine
+                            ExtraDueArray(hCount) = 0
+                            ExtraDueArray(cCount) = SettlementAmount
+                            Exit For
+                        End If
+                    End If
+                Next
+            End If
+        Next       
+        AnswerRTB.Text = Hisab
+    End Sub
+
+    Private Sub ResetBtn_Click(sender As Object, e As EventArgs) Handles ResetBtn.Click        
+        PersonCounter = 0
+        AmountCounter = 0
+        TotalAmount = 0
+        ReDim PersonAmountArray(100, 100)
+        NameCB.Items.Clear()
+        AnswerRTB.Clear()
+        NameCB.Text = "-- Select Person --"
+        AmountTB.Text = "Enter Amount"
+        Status = ""
+    End Sub
+
+    Private Sub NameCB_SelectedIndexChanged(sender As Object, e As EventArgs) Handles NameCB.SelectedIndexChanged
+
+    End Sub
+
+#Region "BackGroundWorker"
+
+    Private Sub BackgroundWorker_DoWork(sender As Object, e As DoWorkEventArgs) Handles BackgroundWorker.DoWork
+        TotalAmount = TotalAmount + 0
+    End Sub
+
+    Private Sub BackgroundWorker_ProgressChanged(sender As Object, e As ProgressChangedEventArgs) Handles BackgroundWorker.ProgressChanged
+
+    End Sub
+
+    Private Sub BackgroundWorker_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs) Handles BackgroundWorker.RunWorkerCompleted
+        AnswerRTB.Text = "TOTAL AMOUNT = " & TotalAmount
+    End Sub
+
+#End Region
+
+
+#Region "ExtraEffects"
+
+    Private Sub AmountTB_TextChanged(sender As Object, e As EventArgs) Handles AmountTB.TextChanged
+        
+    End Sub
+
+    Private Sub AmountTB_Enter(sender As Object, e As EventArgs) Handles AmountTB.Enter
+        If AmountTB.Text = "Enter Amount" Then
+            AmountTB.Text = ""
+        End If
+    End Sub
+
+    Private Sub AmountTB_Leave(sender As Object, e As EventArgs) Handles AmountTB.Leave
+        If AmountTB.Text = "" Then
+            AmountTB.Text = "Enter Amount"
+        End If
+    End Sub
+
+#End Region
+
+End Class
